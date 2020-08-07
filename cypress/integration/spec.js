1f92b0e855fe683d1627995bfe91726103299788
termcaps <avallete@student.42.fr>
2020-08-07 20:24:26 +1000

    refactor(tests): Refactor tests to have every contexts where the bug occurs

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,8 +1,9 @@
 describe('event-triggering', () => {
-    it('should trigger KeyboardEvent with .type', () => {
+    it('should trigger KeyboardEvent with .type inside Cypress event listener', () => {
         cy.visit('./static/minimal-test.html');
-        console.log("test: should trigger KeyboardEvent with .type");
+        console.log("test: should trigger KeyboardEvent with .type inside Cypress event listener");
         // Add event listener
+        let eventObj = {isKeyboardEvent: false};
         cy.get('#test-input').then(jQueryElement => {
             let elemHtml = jQueryElement.get(0);
             elemHtml.addEventListener('keydown', (event) => {
@@ -13,14 +14,17 @@ describe('event-triggering', () => {
                     event constructor: ${event.constructor}
                     event object: 
                 `, event);
+                eventObj.isKeyboardEvent = (event instanceof KeyboardEvent);
             });
         });
         cy.get('#test-input').type('A');
+        cy.wrap(eventObj).its('isKeyboardEvent').should('eq', true);
     });
-    it('should trigger KeyboardEvent with .trigger', () => {
+    it('should trigger KeyboardEvent with .trigger inside Cypress event listener', () => {
         cy.visit('./static/minimal-test.html');
-        console.log("test: should trigger KeyboardEvent with .trigger");
+        console.log("test: should trigger KeyboardEvent with .trigger inside Cypress event listener");
         // Add event listener
+        let eventObj = {isKeyboardEvent: false};
         cy.get('#test-input').then(jQueryElement => {
             let elemHtml = jQueryElement.get(0);
             elemHtml.addEventListener('keydown', (event) => {
@@ -31,6 +35,7 @@ describe('event-triggering', () => {
                     event constructor: ${event.constructor}
                     event object: 
                 `, event);
+                eventObj.isKeyboardEvent = (event instanceof KeyboardEvent);
             });
         });
         cy.get('#test-input').trigger('keydown', {
@@ -39,11 +44,13 @@ describe('event-triggering', () => {
             shiftKey: false,
             ctrlKey: false
         });
+        cy.wrap(eventObj).its('isKeyboardEvent').should('eq', true);
     });
-    it('should trigger KeyboardEvent with .get.then .dispatchEvent on htmlElement', () => {
+    it('should trigger KeyboardEvent with .get.then .dispatchEvent on htmlElement inside Cypress event listener', () => {
         cy.visit('./static/minimal-test.html');
-        console.log("test: should trigger KeyboardEvent with .get.then .dispatchEvent on htmlElement");
+        console.log("test: should trigger KeyboardEvent with .get.then .dispatchEvent on htmlElement inside Cypress event listener");
         // Add event listener
+        let eventObj = {isKeyboardEvent: false};
         cy.get('#test-input').then(jQueryElement => {
             let elemHtml = jQueryElement.get(0);
             elemHtml.addEventListener('keydown', (event) => {
@@ -54,6 +61,7 @@ describe('event-triggering', () => {
                     event constructor: ${event.constructor}
                     event object: 
                 `, event);
+                eventObj.isKeyboardEvent = (event instanceof KeyboardEvent);
             });
         });
         cy.get('#test-input').then(jQueryElement => {
@@ -66,5 +74,118 @@ describe('event-triggering', () => {
             });
             elemHtml.dispatchEvent(kbEvent);
         });
+        cy.wrap(eventObj).its('isKeyboardEvent').should('eq', true);
+    });
+    it('should trigger KeyboardEvent with .type inside html script event listener', () => {
+        cy.visit('./static/minimal-test.html');
+        console.log("test: should trigger KeyboardEvent with .type inside html script event listener");
+        cy.get('#test-input').type('A');
+        cy.get('#result').contains('isKeyboardEvent: true');
+    });
+    it('should trigger KeyboardEvent with .trigger inside html script event listener', () => {
+        cy.visit('./static/minimal-test.html');
+        console.log("test: should trigger KeyboardEvent with .trigger inside html script event listener");
+        cy.get('#test-input').trigger('keydown', {
+            keyCode: 65,
+            which: 65,
+            shiftKey: false,
+            ctrlKey: false
+        });
+        cy.get('#result').contains('isKeyboardEvent: true');
+    });
+    it('should trigger KeyboardEvent with .get.then .dispatchEvent on htmlElement inside html script event listener', () => {
+        cy.visit('./static/minimal-test.html');
+        console.log("test: should trigger KeyboardEvent with .get.then .dispatchEvent on htmlElement inside html script event listener");
+        cy.get('#test-input').then(jQueryElement => {
+            let elemHtml = jQueryElement.get(0);
+            let kbEvent = new KeyboardEvent("keydown", {
+                keyCode: 65,
+                which: 65,
+                shiftKey: false,
+                ctrlKey: false
+            });
+            elemHtml.dispatchEvent(kbEvent);
+        });
+        cy.get('#result').contains('isKeyboardEvent: true');
+    });
+    it('should trigger KeyboardEvent with .type inside both', () => {
+        cy.visit('./static/minimal-test.html');
+        console.log("test: should trigger KeyboardEvent with .type inside both");
+        // Add event listener
+        let eventObj = {isKeyboardEvent: false};
+        cy.get('#test-input').then(jQueryElement => {
+            let elemHtml = jQueryElement.get(0);
+            elemHtml.addEventListener('keydown', (event) => {
+                console.log(`trigerred event show from cypressListener file !
+                    received key: '${event.keyCode}'
+                    is instanceof  KeyboardEvent ? : (${event instanceof KeyboardEvent})
+                    event type: (${event.type})
+                    event constructor: ${event.constructor}
+                    event object: 
+                `, event);
+                eventObj.isKeyboardEvent = (event instanceof KeyboardEvent);
+            });
+        });
+        cy.get('#test-input').type('A');
+        cy.wrap(eventObj).its('isKeyboardEvent').should('eq', true);
+        cy.get('#result').contains('isKeyboardEvent: true');
+    });
+    it('should trigger KeyboardEvent with .trigger inside both', () => {
+        cy.visit('./static/minimal-test.html');
+        console.log("test: should trigger KeyboardEvent with .trigger inside both");
+        // Add event listener
+        let eventObj = {isKeyboardEvent: false};
+        cy.get('#test-input').then(jQueryElement => {
+            let elemHtml = jQueryElement.get(0);
+            elemHtml.addEventListener('keydown', (event) => {
+                console.log(`trigerred event show from cypressListener file !
+                    received key: '${event.keyCode}'
+                    is instanceof  KeyboardEvent ? : (${event instanceof KeyboardEvent})
+                    event type: (${event.type})
+                    event constructor: ${event.constructor}
+                    event object: 
+                `, event);
+                eventObj.isKeyboardEvent = (event instanceof KeyboardEvent);
+            });
+        });
+        cy.get('#test-input').trigger('keydown', {
+            keyCode: 65,
+            which: 65,
+            shiftKey: false,
+            ctrlKey: false
+        });
+        cy.wrap(eventObj).its('isKeyboardEvent').should('eq', true);
+        cy.get('#result').contains('isKeyboardEvent: true');
+    });
+    it('should trigger KeyboardEvent with .get.then .dispatchEvent on htmlElement inside both', () => {
+        cy.visit('./static/minimal-test.html');
+        console.log("test: should trigger KeyboardEvent with .get.then .dispatchEvent on htmlElement inside both");
+        // Add event listener
+        let eventObj = {isKeyboardEvent: false};
+        cy.get('#test-input').then(jQueryElement => {
+            let elemHtml = jQueryElement.get(0);
+            elemHtml.addEventListener('keydown', (event) => {
+                console.log(`trigerred event show from cypressListener file !
+                    received key: '${event.keyCode}'
+                    is instanceof  KeyboardEvent ? : (${event instanceof KeyboardEvent})
+                    event type: (${event.type})
+                    event constructor: ${event.constructor}
+                    event object: 
+                `, event);
+                eventObj.isKeyboardEvent = (event instanceof KeyboardEvent);
+            });
+        });
+        cy.get('#test-input').then(jQueryElement => {
+            let elemHtml = jQueryElement.get(0);
+            let kbEvent = new KeyboardEvent("keydown", {
+                keyCode: 65,
+                which: 65,
+                shiftKey: false,
+                ctrlKey: false
+            });
+            elemHtml.dispatchEvent(kbEvent);
+        });
+        cy.wrap(eventObj).its('isKeyboardEvent').should('eq', true);
+        cy.get('#result').contains('isKeyboardEvent: true');
     });
 })
--- cypress/integration/spec.mouse.js
+++ cypress/integration/spec.mouse.js
@@ -1,26 +1,9 @@
 describe('mouse event-triggering', () => {
-    it('should trigger MouseEvent with .type', () => {
+    it('should trigger MouseEvent with .click inside Cypress event listener', () => {
         cy.visit('./static/minimal-mouse-test.html');
-        console.log("test: should trigger MouseEvent with .click");
+        console.log("test: should trigger MouseEvent with .click inside Cypress event listener");
         // Add event listener
-        cy.get('#test-input').then(jQueryElement => { 
-            let elem = jQueryElement.get(0);
-            elem.addEventListener('mousedown', (event) => {
-                console.log(`trigerred event show from cypressListener file ! 
-                    button clicked: '${event.button}'
-                    is instanceof  MouseEvent ? : (${event instanceof MouseEvent})
-                    event type: (${event.type})
-                    event constructor: ${event.constructor}
-                    event object: 
-                    `, event);
-            });
-        });
-        cy.get('#test-input').click();
-    });
-    it('should trigger MouseEvent with .trigger', () => {
-        cy.visit('./static/minimal-mouse-test.html');
-        console.log("test: should trigger MouseEvent with .trigger");
-        // Add event listener
+        let eventObj = {isMousedEvent: false};
         cy.get('#test-input').then(jQueryElement => {
             let elem = jQueryElement.get(0);
             elem.addEventListener('mousedown', (event) => {
@@ -31,6 +14,28 @@ describe('mouse event-triggering', () => {
                     event constructor: ${event.constructor}
                     event object: 
                     `, event);
+                eventObj.isMouseEvent = (event instanceof MouseEvent);
+            });
+        });
+        cy.get('#test-input').click();
+        cy.wrap(eventObj).its('isMouseEvent').should('eq', true);
+    });
+    it('should trigger MouseEvent with .trigger inside Cypress event listener', () => {
+        cy.visit('./static/minimal-mouse-test.html');
+        console.log("test: should trigger MouseEvent with .trigger inside Cypress event listener");
+        // Add event listener
+        let eventObj = {isMousedEvent: false};
+        cy.get('#test-input').then(jQueryElement => {
+            let elem = jQueryElement.get(0);
+            elem.addEventListener('mousedown', (event) => {
+                console.log(`trigerred event show from cypressListener file ! 
+                    button clicked: '${event.button}'
+                    is instanceof  MouseEvent ? : (${event instanceof MouseEvent})
+                    event type: (${event.type})
+                    event constructor: ${event.constructor}
+                    event object: 
+                    `, event);
+                eventObj.isMouseEvent = (event instanceof MouseEvent);
             });
 
         });
@@ -39,11 +44,13 @@ describe('mouse event-triggering', () => {
             shiftKey: false,
             ctrlKey: false
         });
+        cy.wrap(eventObj).its('isMouseEvent').should('eq', true);
     });
-    it('should trigger MouseEvent with .get.then .dispatchEvent on htmlElement', () => {
+    it('should trigger MouseEvent with .get.then .dispatchEvent on htmlElement inside Cypress event listener', () => {
         cy.visit('./static/minimal-mouse-test.html');
-        console.log("test: should trigger MouseEvent with .get.then .dispatchEvent on htmlElement");
+        console.log("test: should trigger MouseEvent with .get.then .dispatchEvent on htmlElement inside Cypress event listener");
         // Add event listener
+        let eventObj = {isMousedEvent: false};
         cy.get('#test-input').then(jQueryElement => {
             let elem = jQueryElement.get(0);
             elem.addEventListener('mousedown', (event) => {
@@ -54,6 +61,7 @@ describe('mouse event-triggering', () => {
                     event constructor: ${event.constructor}
                     event object: 
                     `, event);
+                eventObj.isMouseEvent = (event instanceof MouseEvent);
             });
 
         });
@@ -66,5 +74,116 @@ describe('mouse event-triggering', () => {
             });
             elemHtml.dispatchEvent(mEvent);
         });
+        cy.wrap(eventObj).its('isMouseEvent').should('eq', true);
+    });
+    it('should trigger MouseEvent with .click inside html script event listener', () => {
+        cy.visit('./static/minimal-mouse-test.html');
+        console.log("test: should trigger MouseEvent with .click inside html script event listener");
+        cy.get('#test-input').click();
+        cy.get('#result').contains('isMouseEvent: true');
+    });
+    it('should trigger MouseEvent with .trigger inside html script event listener', () => {
+        cy.visit('./static/minimal-mouse-test.html');
+        console.log("test: should trigger MouseEvent with .trigger inside html script event listener");
+        cy.get('#test-input').trigger('mousedown', {
+            button: 0,
+            shiftKey: false,
+            ctrlKey: false
+        });
+        cy.get('#result').contains('isMouseEvent: true');
+    });
+    it('should trigger MouseEvent with .get.then .dispatchEvent on htmlElement inside html script event listener', () => {
+        cy.visit('./static/minimal-mouse-test.html');
+        console.log("test: should trigger MouseEvent with .get.then .dispatchEvent on htmlElement inside html script event listener");
+        cy.get('#test-input').then(jQueryElement => {
+            let elemHtml = jQueryElement.get(0);
+            let mEvent = new MouseEvent("mousedown", {
+                button: 0,
+                shiftKey: false,
+                ctrlKey: false
+            });
+            elemHtml.dispatchEvent(mEvent);
+        });
+        cy.get('#result').contains('isMouseEvent: true');
+    });
+    it('should trigger MouseEvent with .click inside both', () => {
+        cy.visit('./static/minimal-mouse-test.html');
+        console.log("test: should trigger MouseEvent with .click inside both");
+        // Add event listener
+        let eventObj = {isMousedEvent: false};
+        cy.get('#test-input').then(jQueryElement => {
+            let elem = jQueryElement.get(0);
+            elem.addEventListener('mousedown', (event) => {
+                console.log(`trigerred event show from cypressListener file ! 
+                    button clicked: '${event.button}'
+                    is instanceof  MouseEvent ? : (${event instanceof MouseEvent})
+                    event type: (${event.type})
+                    event constructor: ${event.constructor}
+                    event object: 
+                    `, event);
+                eventObj.isMouseEvent = (event instanceof MouseEvent);
+            });
+        });
+        cy.get('#test-input').click();
+        cy.wrap(eventObj).its('isMouseEvent').should('eq', true);
+        cy.get('#result').contains('isMouseEvent: true');
+    });
+    it('should trigger MouseEvent with .trigger inside both', () => {
+        cy.visit('./static/minimal-mouse-test.html');
+        console.log("test: should trigger MouseEvent with .trigger inside both");
+        // Add event listener
+        let eventObj = {isMousedEvent: false};
+        cy.get('#test-input').then(jQueryElement => {
+            let elem = jQueryElement.get(0);
+            elem.addEventListener('mousedown', (event) => {
+                console.log(`trigerred event show from cypressListener file ! 
+                    button clicked: '${event.button}'
+                    is instanceof  MouseEvent ? : (${event instanceof MouseEvent})
+                    event type: (${event.type})
+                    event constructor: ${event.constructor}
+                    event object: 
+                    `, event);
+                eventObj.isMouseEvent = (event instanceof MouseEvent);
+            });
+
+        });
+        cy.get('#test-input').trigger('mousedown', {
+            button: 0,
+            shiftKey: false,
+            ctrlKey: false
+        });
+        cy.wrap(eventObj).its('isMouseEvent').should('eq', true);
+        cy.get('#result').contains('isMouseEvent: true');
+    });
+    it('should trigger MouseEvent with .get.then .dispatchEvent on htmlElement inside both', () => {
+        cy.visit('./static/minimal-mouse-test.html');
+        console.log("test: should trigger MouseEvent with .get.then .dispatchEvent on htmlElement inside both");
+        // Add event listener
+        let eventObj = {isMousedEvent: false};
+        cy.get('#test-input').then(jQueryElement => {
+            let elem = jQueryElement.get(0);
+            elem.addEventListener('mousedown', (event) => {
+                console.log(`trigerred event show from cypressListener file ! 
+                    button clicked: '${event.button}'
+                    is instanceof  MouseEvent ? : (${event instanceof MouseEvent})
+                    event type: (${event.type})
+                    event constructor: ${event.constructor}
+                    event object: 
+                    `, event);
+                eventObj.isMouseEvent = (event instanceof MouseEvent);
+            });
+
+        });
+        cy.get('#test-input').then(jQueryElement => {
+            let elemHtml = jQueryElement.get(0);
+            let mEvent = new MouseEvent("mousedown", {
+                button: 0,
+                shiftKey: false,
+                ctrlKey: false
+            });
+            elemHtml.dispatchEvent(mEvent);
+        });
+        cy.wrap(eventObj).its('isMouseEvent').should('eq', true);
+        cy.get('#result').contains('isMouseEvent: true');
     });
 })
